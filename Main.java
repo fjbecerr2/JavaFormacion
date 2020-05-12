fa0b453c0673cc4a52ae79518a0fe37b8b95ee42
Francisco J. Becerra <fjbecerr2@gmail.com>
2020-05-12 10:02:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  /**
+    Autor: Fco. J. Becerra
+    Date: 12/05/2020
+    Version: 1.0
+    Update-Date:
+    Description:
+      Calcular la paga neta de un trabajador
+    Param:
+      horas
+      tarifa
+      impuestos
+    Return:
+      paga
+  **/
+  public static void Ejer1_1(){
+
+  }
+}
